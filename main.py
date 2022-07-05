14f1cc6f1370cb25073abe410d63fa0f84735643
Casperento <44746868+Casperento@users.noreply.github.com>
2022-07-05 00:05:06 -0300

    start message

--- main.py
+++ main.py
@@ -82,6 +82,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     interval = 12*3600  # em segundos
     logging.info(f"Configurando job_queue para enviar mensagens a cada {interval} segundos...")
     context.job_queue.run_repeating(coroutine, interval, chat_id=CHAT_ID, name=str(CHAT_ID))
+    await update.message.reply_text("Bot inicializado!")
 
 
 async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
